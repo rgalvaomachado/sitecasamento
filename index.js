bcd62338a7f90db205bf38e3bd6ad7b07c6c6c9f
Rômulo Galvão Machado <52431713+rgalvaomachado@users.noreply.github.com>
2023-09-18 14:51:30 -0300

    audio

--- index.js
+++ index.js
@@ -38,6 +38,34 @@ function showIndicacaoNoivos(){
     $('#indicacao_noivos').show();
 }
 
+function play(){
+    $('.fa-play').hide();
+    $('.fa-pause').show();
+    let player = $('#player');
+    player[0].play();
+}
+
+function pause(){
+    $('.fa-play').show();
+    $('.fa-pause').hide();
+    let player = $('#player');
+    player[0].pause();
+}
+
+function mute(){
+    $('.fa-volume-mute').hide();
+    $('.fa-volume-up').show();
+    let player = $('#player');
+    player[0].muted = true;
+}
+
+function volume(){
+    $('.fa-volume-mute').show();
+    $('.fa-volume-up').hide();
+    let player = $('#player');
+    player[0].muted = false;
+}
+
 
 function makeTimer() {
     var endTime = new Date("March 30, 2024 16:30:00 GMT-03:00");     
